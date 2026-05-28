529f65852dd57db6e940438d443ef0f782d73657
alton skyline stay <budiantoandrias@gmail.com>
2026-05-28 19:18:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("ALTON SKYLINE READY");
+GANTI TOTAL ISI "script.js"
+
+function adminAlert(){
+
+alert(
+"Admin panel premium akan diaktifkan pada update berikutnya."
+);
+
+}
+
+console.log("ALTON SKYLINE PREMIUM READY");
